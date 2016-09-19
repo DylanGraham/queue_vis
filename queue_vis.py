ff962bd2fad14a155d9cadacb2c0a68b48f39624
Dylan Graham <dylan.graham@gmail.com>
2016-09-19 11:31:06 +1000

    Initial commit

--- queue_vis.py
+++ queue_vis.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+data = {
+    'bacillus': (207, 1242),
+    'bulls1k': (59, 70),
+    'cowfert': (1, 1),
+    'cows10k': (5, 51),
+    'das': (17, 94),
+    'pulsemarker': (2, 24)
+}
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def graph():
+    return render_template('graph1.html', data=data)
 
 
 if __name__ == '__main__':
