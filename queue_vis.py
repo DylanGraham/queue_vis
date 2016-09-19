d891c6b3c9f1dd09719399b8437a3d04536f7d8c
Dylan Graham <dylan.graham@gmail.com>
2016-09-19 13:41:40 +1000

    remove dummy data

--- queue_vis.py
+++ queue_vis.py
@@ -2,19 +2,10 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-data_active_procs = {
-    'bacillus': 1242,
-    'bulls1k': 70,
-    'cowfert': 1,
-    'cows10k': 51,
-    'das': 94,
-    'pulsemarker': 24,
-}
-
 
 @app.route('/')
 def graph():
-    return render_template('graph1.html', data=data_active_procs)
+    return render_template('graph1.html')
 
 
 if __name__ == '__main__':
