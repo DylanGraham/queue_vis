5f236ebb983bc47378a704ec75c783ce59b37ba9
Dylan Graham <dylan.graham@gmail.com>
2016-09-19 13:36:41 +1000

    d3 v3

--- queue_vis.py
+++ queue_vis.py
@@ -2,19 +2,19 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-data = {
-    'bacillus': (207, 1242),
-    'bulls1k': (59, 70),
-    'cowfert': (1, 1),
-    'cows10k': (5, 51),
-    'das': (17, 94),
-    'pulsemarker': (2, 24)
+data_active_procs = {
+    'bacillus': 1242,
+    'bulls1k': 70,
+    'cowfert': 1,
+    'cows10k': 51,
+    'das': 94,
+    'pulsemarker': 24,
 }
 
 
 @app.route('/')
 def graph():
-    return render_template('graph1.html', data=data)
+    return render_template('graph1.html', data=data_active_procs)
 
 
 if __name__ == '__main__':
